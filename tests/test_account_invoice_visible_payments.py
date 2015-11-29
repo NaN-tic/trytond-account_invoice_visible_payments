39dbd76ddd7f5f0ccb68b09bbfe73f6a840a7614
resteve <resteve@zikzakmedia.com>
2015-11-29 11:42:42 +0100

    pyflakes

--- tests/test_account_invoice_visible_payments.py
+++ tests/test_account_invoice_visible_payments.py
@@ -2,7 +2,7 @@
 # This file is part of the account_invoice_visible_payments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import test_depends
 import trytond.tests.test_tryton
 import unittest
 
