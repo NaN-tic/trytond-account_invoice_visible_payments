6179c19fd65027581e9e413b1d33249fabb2b2ef
resteve <resteve@zikzakmedia.com>
2015-12-01 10:58:51 +0100

    Tests

--- tests/test_account_invoice_visible_payments.py
+++ tests/test_account_invoice_visible_payments.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the account_invoice_visible_payments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceVisiblePaymentsTestCase(unittest.TestCase):
+class AccountInvoiceVisiblePaymentsTestCase(ModuleTestCase):
     'Test Account Invoice Visible Payments module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_visible_payments')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_visible_payments'
 
 
 def suite():
